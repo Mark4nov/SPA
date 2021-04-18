cf499a5c8f8622c97e3d731c02db9250311450a2
Jesús Marcano <mpjeduardo@outlook.com>
2021-04-18 18:01:30 -0400

    Proyecto finalizado

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -1,17 +1,21 @@
-const Character = () =>{
+import getData from '../utils/getData';
+import getHash from '../utils/getHash';
+
+const Character = async() =>{
+    const data = await getData(getHash());
     const view= `
     <div class="Character-inner">
         <article class="Characters-card">
-            <img src="image" alt="CharacterName"/>
-            <h2>Character Name</h2>
+            <img src="${data.image}" alt="${data.name}"/>
+            <h2>${data.name}</h2>
         </article>
         <article class="Characters-card">
-            <h3>Episodes:</h3>
-            <h3>Status:</h3>
-            <h3>Species:</h3>
-            <h3>Gender:</h3>
-            <h3>Origin:</h3>
-            <h3>Last Location:</h3>
+            <h3>Episodes: ${data.episode.length}</h3>
+            <h3>Status: ${data.status}</h3>
+            <h3>Species: ${data.species}</h3>
+            <h3>Gender: ${data.gender}</h3>
+            <h3>Origin: ${data.origin.name}</h3>
+            <h3>Last Location: ${data.location.name}</h3>
         </article>
     </div>
     `;
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path'); //Nos permite acceder a donde estámos en las carpetas. Ya sea en local o en la nube.
 const HtmlWebpackPlugin = require('html-webpack-plugin'); //Archivo necesario para trabajar con HTML.
+const CopyWebPackPlugin = require('copy-webpack-plugin');
 
 module.exports = {  //Aquí se encuentra toda la configuración de lo que va a suceder. Modulo para exportar.
     entry: './src/index.js', //Punto de entrada con su dirección.Aquí vive el código inicial y de aquí parte al desarrollo.
@@ -22,12 +23,16 @@ module.exports = {  //Aquí se encuentra toda la configuración de lo que va a s
         ]
     },
     plugins: [  //Establecemos los plugins que vamos a utilizar
-        new HtmlWebpackPlugin([    //Permite trabajar con los archivos HTML
+        new HtmlWebpackPlugin(    //Permite trabajar con los archivos HTML
             {
                 inject: true,   //Cómo vamos a inyectar un valor a un archivo HTML.
                 template: './public/index.html',    //Dirección donde se encuentra el template principal
                 filename: './index.html'    //El nombre que tendrá el archivo
             }
-        ])
+        ),
+        new CopyWebPackPlugin({patterns: [{
+            from: './src/styles/styles.css',
+            to: ''
+        }]})
     ]
 }
