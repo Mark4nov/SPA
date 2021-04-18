a14db8201e2334d6cfef1e6263b2b0817b01c3e2
Jesús Marcano <mpjeduardo@outlook.com>
2021-04-18 10:44:00 -0400

    Añadida la pagina error 404

---
+++ src/pages/Error404.js
@@ -0,0 +1,10 @@
+const Error404 = () =>{
+    const view = `
+    <div class="Error404">
+        <h2>Error 404: No se encontró un carajo.</h2>
+    </div>
+    `;
+    return view;
+};
+
+export default Error404;
